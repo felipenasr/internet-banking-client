9e08dc49dcce93d2a72ba45b352df662a90e86f5
Rafael Paiva <rpaivabr@gmail.com>
2018-01-25 17:15:40 -0200

    Criado dado estático no componente, ngFor nas transferencias do extrato

--- src/app/extract/extract.component.ts
+++ src/app/extract/extract.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
 
 @Component({
   selector: 'app-extract',
@@ -7,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ExtractComponent implements OnInit {
 
+  listaTransfers = [
+    {dia: 12, mes: 'Jan', origem: 'MercadoCar Faria Lima', tipo: 'Cartão de Crédito', valor: 185},
+    {dia: 13, mes: 'Jan', origem: 'Pedro Carlos', tipo: 'Transferência: TEF', valor: 250},
+    {dia: 14, mes: 'Jan', origem: 'José Fernando', tipo: 'Transferência: DOC', valor: 750},
+    {dia: 14, mes: 'Jan', origem: 'BK Shopping Eldorado', tipo: 'Cartão de Crédito', valor: 75} 
+  ]
 
   constructor() { }
 
