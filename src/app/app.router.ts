e0e22e058f22e68c9399c64675bdcd87c5f90c9f
Raphael Narasaki <narasakir@gmail.com>
2018-01-25 01:28:33 -0200

    View do componente de extrato

--- src/app/app.router.ts
+++ src/app/app.router.ts
@@ -3,12 +3,14 @@ import { Routes, RouterModule } from '@angular/router'
 import { LoginComponent } from './login/login.component'
 import { DashboardComponent } from './dashboard/dashboard.component'
 import { TransferComponent } from './transfer/transfer.component'
+import { ExtractComponent } from './extract/extract.component';
 
 const rotasApp: Routes = [
     { path: '', component: LoginComponent},
     { path: 'login', component: LoginComponent },
     { path: 'dashboard', component: DashboardComponent },
     { path: 'transfer', component: TransferComponent },
+    { path: 'extract', component: ExtractComponent },
     { path: '**', redirectTo: '/' }
 ]
 
