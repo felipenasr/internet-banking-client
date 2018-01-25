19fbf0ceb0e568e1a6ee0da82c923c7a1c885161
Rafael Paiva <rpaivabr@gmail.com>
2018-01-24 22:08:15 -0200

    separado sidenav e header, serviços no provider do app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,25 +2,34 @@ import { BrowserModule } from '@angular/platform-browser'
 import { NgModule } from '@angular/core'
 import { RotasModule } from './app.router'
 
-
 import { AppComponent } from './app.component'
 import { LoginComponent } from './login/login.component'
 import { DashboardComponent } from './dashboard/dashboard.component'
 import { TransferComponent } from './transfer/transfer.component'
 
+import { HeaderComponent } from './shared/header/header.component'
+import { SidenavComponent } from './shared/sidenav/sidenav.component'
+
+import { ApiService } from './shared/services/api.service'
+import { LocalStorageService } from './shared/services/local-storage.service'
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     DashboardComponent,
-    TransferComponent
+    TransferComponent,
+    HeaderComponent,
+    SidenavComponent
   ],
   imports: [
     BrowserModule,
     RotasModule
   ],
-  providers: [],
+  providers: [
+    ApiService,
+    LocalStorageService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
