3dc32b06c14b2514c0ec829c7d755f886b5bc67a
Raphael Narasaki <narasakir@gmail.com>
2018-01-24 23:15:58 -0200

    Ajuste no sidebar-menu e cards

--- html-estatico/assets/js/scripts.js
+++ html-estatico/assets/js/scripts.js
@@ -1,5 +1,6 @@
-$('.btn-sidebar').on('click', function () {
+$('.hamburguer').on('click', function () {
     // open sidebar
     $('.sidebar-menu').toggleClass('full');
+    $(this).toggleClass('open');
     $('.main').toggleClass('full');
 });
