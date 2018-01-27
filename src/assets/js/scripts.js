c61b835ad6052a35bba048c4ebe492dac29a3398
Raphael Narasaki <narasakir@gmail.com>
2018-01-27 15:21:35 -0200

    Organizar o CSS

--- src/assets/js/scripts.js
+++ src/assets/js/scripts.js
@@ -1,5 +1,6 @@
 $('.hamburguer').on('click', function () {
     // open sidebar
+    console.log('asdcs')
     $('.sidebar-menu').toggleClass('full');
     $(this).toggleClass('open');
     $('.main').toggleClass('full');
